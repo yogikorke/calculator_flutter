e909b6727dd0668e2a17a092587e2cd888e7ffa0
Yogesh Korke <yogikorke91@gmail.com>
2022-12-11 14:04:09 +0530

    added test interstitial ads

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:calculator/views/home_page.dart';
 import 'package:calculator/widgets/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -2,12 +2,61 @@ import 'package:calculator/controllers/calculator_controller.dart';
 import 'package:calculator/widgets/body_container.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import '../widgets/switchThemeButton.dart';
 import '../widgets/theme.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final CalculatorController _controller = Get.put(CalculatorController());
+  InterstitialAd? _interstitialAd;
+
+  @override
+  void initState() {
+    super.initState();
+
+    InterstitialAd.load(
+        adUnitId: 'ca-app-pub-3940256099942544/1033173712',
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            // Keep a reference to the ad so you can show it later.
+            _interstitialAd = ad;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            debugPrint('InterstitialAd failed to load: $error');
+          },
+        )).then((value) => {
+          _interstitialAd?.fullScreenContentCallback =
+              FullScreenContentCallback(
+            onAdShowedFullScreenContent: (InterstitialAd ad) =>
+                debugPrint('%ad onAdShowedFullScreenContent.'),
+            onAdDismissedFullScreenContent: (InterstitialAd ad) {
+              debugPrint('$ad onAdDismissedFullScreenContent.');
+              ad.dispose();
+            },
+            onAdFailedToShowFullScreenContent:
+                (InterstitialAd ad, AdError error) {
+              debugPrint('$ad onAdFailedToShowFullScreenContent: $error');
+              ad.dispose();
+            },
+            onAdImpression: (InterstitialAd ad) =>
+                debugPrint('$ad impression occurred.'),
+            onAdClicked: (ad) => debugPrint('$ad ad clicked.'),
+          )
+        });
+
+    Future.delayed(const Duration(seconds: 60), () {
+      debugPrint('showing ad');
+      _interstitialAd?.show();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
